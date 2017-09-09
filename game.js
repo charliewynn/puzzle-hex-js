1d5e1e15821d41879ccc3224b1d3801b48f57782
Charlie Wynn <charliewynn@gmail.com>
2017-09-08 19:31:32 -0500

    debugging pixel ratio

--- game.js
+++ game.js
@@ -4,9 +4,18 @@ function didLoad(){
 	var size = pt(window.innerWidth, window.innerHeight);
 	
 	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext('2d');
+	console.log("dpr = " + window.devicePixelRatio);
+	alert("dpr = " + window.devicePixelRatio);
+	if(window.devicePixelRatio == 2){
+		console.log("did 2x");
+		size.x /=2;
+		size.y /=2;
+		context.scale(2,2);
+	}
 	canvas.setAttribute('width', size.x);
 	canvas.setAttribute('height', size.y);
 	var game = {};
-	var draw = new Drawing(size, canvas.getContext("2d"));
+	var draw = new Drawing(size, context);
 	draw.render(game);
 }	
