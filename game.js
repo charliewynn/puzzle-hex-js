105ddd869025e96fbba7a56895306f4259e390f7
Charlie Wynn <charliewynn@gmail.com>
2017-09-15 13:03:10 -0500

    Update game.js

--- game.js
+++ game.js
@@ -29,7 +29,7 @@ function didLoad(){
 
 function Game(width, height, canvas){
 		var game = this;
-		game.ver = '0.01';
+		game.ver = '0.02';
 
 		this.Geometry = new Geometry();
 		var size = new Point(window.innerWidth, window.innerHeight);
