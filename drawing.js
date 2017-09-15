f5b0d0d9d7f4852f1c9c969198f51f8f2ce9377c
Charlie Wynn <charliewynn@gmail.com>
2017-09-15 13:02:54 -0500

    Update drawing.js

--- drawing.js
+++ drawing.js
@@ -8,7 +8,7 @@ function Drawing(canvas, context)
 	this.render = function(game){
 		if(this.ClearCountDown > 0) this.ClearCountDown--;
 		this.canvas.width = this.canvas.width;
-		this.strokeText(new Point(0,10), game.ver, 'black', '10px sans-serif');
+		this.strokeText(new Point(2,12), game.ver, 'black', '10px sans-serif');
 		for(h in game.Hexs)
 			this.hex(game.Hexs[h], hexSize, this.toBeCleared.indexOf(game.Hexs[h]) < 0, game.debug);
 
